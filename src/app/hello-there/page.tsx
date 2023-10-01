62b39396fc6a6a53ec5e16e77ee3088c01aca638
John <johnnyz88@gmail.com>
2023-10-01 16:21:45 +1100

    init commit

---
+++ src/app/hello-there/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <div>Greetings!</div>
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+}
